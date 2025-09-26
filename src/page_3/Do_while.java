3c63498ae9aa7b1cb58ee6cd856ed0fe05a571c6
navya-sharath <Navyakrishnasharath@gmail.com>
2025-09-25 20:44:09 -0500

    loop

---
+++ src/page_3/Do_while.java
@@ -0,0 +1,17 @@
+package page_3;
+
+public class Do_while {
+
+	public static void main(String[] args) {
+		int num=2;
+		int i=1;
+		while(i<=10) {
+			System.out.println(num+"*"+i+"="+(num*i));
+			i++;
+			
+		}
+		
+		
+	}
+
+}
---
+++ src/page_4/Encapsulation_example2.java
@@ -0,0 +1,15 @@
+package page_4;
+
+public class Encapsulation_example2 {
+
+	public static void main(String[] args) {
+		Encapsulation_example ex=new Encapsulation_example();
+		ex.setName("navya");
+		System.out.println(ex.getName());
+		
+		ex.setAge(39);
+		System.out.println(ex.getAge());
+
+	}
+
+}
