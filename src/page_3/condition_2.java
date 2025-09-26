5688a86c33ae2961cf8c4a6aef5c036153105f51
navya-sharath <Navyakrishnasharath@gmail.com>
2025-09-25 20:38:25 -0500

    if_statement

---
+++ src/page_3/condition_2.java
@@ -0,0 +1,18 @@
+package page_3;
+
+public class condition_2 {
+
+	public static void main(String[] args) {
+		int num =-23;
+		if (num<0) {
+			System.out.println(" this number is " + num+ " negative ");
+		}
+		else if (num>0) {
+			System.out.println("this number is " + num+ " positive ");
+		}
+		System.out.println(" thank you");
+
+	}
+	
+
+}
